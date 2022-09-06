2e27da195ad50a06491ce9de705af36e6dadd607
CarlosLincon <carloslincon29@gmail.com>
2022-09-06 08:50:51 -0300

    Conclusão do projeto

--- src/components/PaginaPadrao/index.tsx
+++ src/components/PaginaPadrao/index.tsx
@@ -1,8 +1,11 @@
 import styles from "./PaginaPadrao.module.scss";
 import { Outlet } from "react-router-dom";
 import stylesTema from "styles/Tema.module.scss";
+import React from "react";
 
-export default function Header() {
+export default function Header(
+  //{ children }: { children?: React.ReactNode }
+) {
   return (
     <>
       <header className={styles.header}>
@@ -12,8 +15,9 @@ export default function Header() {
       </header>
       <div className={stylesTema.container}>
         <Outlet />
+        {/* {children} */}
       </div>
-      
+
     </>
   );
 }
